c66db4005abf08722e791a8f83f8b732a3a7bed6
rayouxinno <rayouxinnovations@gmail.com>
2025-10-26 05:57:19 +0500

    Update index.production.ts

--- server/index.production.ts
+++ server/index.production.ts
@@ -63,11 +63,7 @@ app.use((req, res, next) => {
   serveStatic(app);
 
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  server.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
